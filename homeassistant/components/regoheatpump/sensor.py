6883a3a653d99d4d18b701d89d631cd2b1be7ad4
Boris Krivonog <boris.krivonog@inova.si>
2025-03-05 21:20:00 +0000

    added sensors

--- homeassistant/components/regoheatpump/sensor.py
+++ homeassistant/components/regoheatpump/sensor.py
@@ -3,6 +3,8 @@
 import logging
 
 from pyrego600 import Register, RegoError, Type
+from pyrego600.identifiers import Identifiers
+from pyrego600.register_factory import RegisterFactory
 
 from homeassistant.components.sensor import SensorDeviceClass, SensorEntity, timedelta
 from homeassistant.const import UnitOfTemperature
@@ -23,6 +25,12 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Test."""
+    register = RegisterFactory.lastError(Identifiers.LAST_ERROR)
+    async_add_entities(
+        [RegoLastErrorEntity(entry, register)],
+        update_before_add=True,
+    )
+
     async_add_entities(
         (
             RegoSensorEntity(entry, register)
@@ -51,6 +59,21 @@ class RegoSensorEntity(SensorEntity, RegoEntity):
             self._attr_entity_registry_enabled_default = (
                 self._attr_native_value is not None
             )
+        except (OSError, RegoError) as e:
+            self._attr_available = False
+            _LOGGER.warning("Reading %s failed due %s", self._register.identifier, e)
+
+
+class RegoLastErrorEntity(SensorEntity, RegoEntity):
+    """An entity using CoordinatorEntity."""
+
+    async def async_update(self) -> None:
+        """Boo."""
+        try:
+            last_error = await self._heat_pump.read(self._register)
+            self._attr_native_value = last_error[0]
+            self.extra_state_attributes = {"timestamp": last_error[1]}
+            self._attr_available = True
         except (OSError, RegoError) as e:
             self._attr_available = False
             _LOGGER.warning("Reading %s failed due %s", self._register.identifier, e)
